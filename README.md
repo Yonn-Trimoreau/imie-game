914146f05c25a9474def3c6b060902ca61682d03
Yonn Trimoreau <trimoreau.yonn@gmail.com>
2015-07-18 12:54:43 +0200

    Initialized Symfony

---
+++ README.md
@@ -0,0 +1,4 @@
+imie-game
+=========
+
+A Symfony project created on July 18, 2015, 12:25 am.
---
+++ src/Imie/GameBundle/ImieGameBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Imie\GameBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ImieGameBundle extends Bundle
+{
+}
