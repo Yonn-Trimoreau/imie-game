2e748bda72f8d1d5b43da3b6480783eb29ff00ab
Yonn Trimoreau <trimoreau.yonn@gmail.com>
2015-07-19 11:33:29 +0200

    Login done

--- src/Imie/GameBundle/Controller/HomeController.php
+++ src/Imie/GameBundle/Controller/HomeController.php
@@ -9,12 +9,29 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ImieGameBundle:Home:index.html.twig');
+        $playerId = $this->get('session')->get('playerid');
+        if(isset($playerId) && !empty($playerId)){
+            return new RedirectResponse($this->generateUrl('imie_game_gamepage'));
+        }
+        else{
+            return $this->render('ImieGameBundle:Home:index.html.twig');
+        }
+    }
+
+    public function loginAction()
+    {
+        $playerRepo = $this->getDoctrine()->getManager()->getRepository('ImieGameBundle:Player');
+
+        $playerId = $playerRepo->createIfNotExists($this->get('request')->request->get('playername'));
+
+        $this->get('session')->set('playerid',$playerId);
+
+        return new RedirectResponse($this->generateUrl('imie_game_gamepage'));
     }
 
     public function logoutAction()
     {
-        $this->get('session')->remove('playername');
+        $this->get('session')->remove('playerid');
 
         return new RedirectResponse($this->generateUrl('imie_game_homepage'));
     }
