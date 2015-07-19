7228df64d4c1fe8511592858589b57001eab87a8
Yonn Trimoreau <trimoreau.yonn@gmail.com>
2015-07-19 07:30:33 +0200

    Enhanced base layout

--- src/Imie/GameBundle/Controller/HomeController.php
+++ src/Imie/GameBundle/Controller/HomeController.php
@@ -3,11 +3,19 @@
 namespace Imie\GameBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
         return $this->render('ImieGameBundle:Home:index.html.twig');
+    }
+
+    public function logoutAction()
+    {
+        $this->get('session')->remove('playername');
+
+        return new RedirectResponse($this->generateUrl('imie_game_homepage'));
     }
 }
